3defc7cbb3f0c4627247fabf7f7466b8689a378c
cunha_isa <cunhaisabellm@gmail.com>
2019-01-31 22:32:57 +0000

    added prettier and configs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { hot } from "react-hot-loader";
+import React from 'react';
+import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import("./Warning"))
+const Warning = React.lazy(() => import('./Warning'));
 
-class App extends React.Component{
-    state = {
-        count: 0
-    }
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-    _increment = () => {
-        this.setState(state => ({count: state.count + 1}));
-    };
+  _increment = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
 
-    _decrement = () => {
-        this.setState(state => ({count: state.count - 1}));
-    };
+  _decrement = () => {
+    this.setState(state => ({ count: state.count - 1 }));
+  };
 
-    render() {
-        const {count} = this.state;
-        return (
-            <div>
-                <h1>Hello World!!</h1>
-                <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
-                <button onClick={this._increment}>+</button>
-                <button onClick={this._decrement}>-</button>
-                {count > 10 ?
-                <React.Suspense fallback={null}>
-                    <Warning />
-                </React.Suspense>
-                : null}
-            </div>
-        )
-    };
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Hello World!!</h1>
+        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
+        <button onClick={this._increment}>+</button>
+        <button onClick={this._decrement}>-</button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default hot(module)(App);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,12 @@
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.config.base");
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
-   mode: "development",
-   devServer: {
-       port: 9000
-   },
-   devtool: "source-map"
-   // map o code no browser to source code par conseguir
-   // fazer debugger corretamente
+  mode: 'development',
+  devServer: {
+    port: 9000
+  },
+  devtool: 'source-map'
+  // map o code no browser to source code par conseguir
+  // fazer debugger corretamente
 });
