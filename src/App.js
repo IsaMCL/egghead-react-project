2ca7044331f1b10096b7906bc17a283b50c092d4
cunha_isa <cunhaisabellm@gmail.com>
2019-01-31 23:12:54 +0000

    add pre-commit hooks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 
 const Warning = React.lazy(() => import('./Warning'))
-const a = "A";
 
 class App extends React.Component {
   state = {
